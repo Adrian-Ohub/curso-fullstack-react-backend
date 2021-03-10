ee2e91d8bb7b6f34c147e5eafa9e9cab7dd2ed00
Adrian-Ohub <63118713+Adrian-Ohub@users.noreply.github.com>
2021-03-10 14:44:33 +0100

    3.6 pb backend

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     name: "Arto Hellas",
@@ -27,6 +29,46 @@ let persons = [
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+//Realiza la accion directamente
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+const generateId = () => Math.floor(Math.random() * (1001 - 0)) + 0;
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response
+      .status(400)
+      .json({ error: "The name or number is missing" });
+  } else if (persons.find((person) => person.name === body.name)) {
+    return response
+      .status(400)
+      .json({ error: "The name already exist in the phonebook" });
+  } else {
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    };
+    persons = persons.concat(person);
+    response.json(person);
+  }
 });
 
 app.get("/info", (request, response) => {
