7a25590ba93138120bc67494f60d4be6317a1628
Adrian-Ohub <63118713+Adrian-Ohub@users.noreply.github.com>
2021-03-12 11:28:27 +0100

    backend heroku config

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 let persons = [
@@ -81,7 +83,8 @@ app.get("/info", (request, response) => {
   response.send(info);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
